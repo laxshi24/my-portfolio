b7da75aff27b8f776ce853642163fa6001f57a46
laxshi24 <laxshisiva@gmail.com>
2025-02-11 16:53:34 +0530

    docbook

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,11 +3,18 @@ import proj1 from '../assets/project1.jpg';
 import food from '../assets/food.png'
 import grocery from '../assets/grocery.avif'
 import logistic from '../assets/project_logistic.jpg';
+import docBooker from '../assets/docBooker.jpg'
 import { motion } from 'framer-motion';
 
 const Projects = () => {
   const [isGymModalOpen, setGymModalOpen] = useState(false);
   const [isLogisticModalOpen, setLogisticModalOpen] = useState(false);
+  const [isDocBookerModalOpen, setDocBookerModalOpen] = useState(false);
+
+  const handleDocBookerModalToggle = () => {
+    setDocBookerModalOpen(!isDocBookerModalOpen);
+  };
+
 
   const handleGymModalToggle = () => {
     setGymModalOpen(!isGymModalOpen);
@@ -16,6 +23,9 @@ const Projects = () => {
   const handleLogisticModalToggle = () => {
     setLogisticModalOpen(!isLogisticModalOpen);
   };
+
+  
+  
 
   return (
     <div className="max-w-[1200px] mx-auto p-5" id="work">
@@ -83,15 +93,17 @@ const Projects = () => {
       </div>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {/* Gym Frontend App */}
-        <div>
+
+         {/* DocBooker App */}
+
+         <div>
           <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative">
-            <img src={proj1} alt="Gym Frontend" />
+            <img src={docBooker} alt="Gym Frontend" />
             <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
               <span className="text-2xl font-bold text-white tracking-wider">Project</span>
               <div className="pt-8 text-center">
                 <button
-                  onClick={handleGymModalToggle}
+                  onClick={handleDocBookerModalToggle}
                   className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
                 >
                   Live
@@ -100,15 +112,14 @@ const Projects = () => {
             </div>
           </div>
           <br />
-          <a
-            href="https://gymfrontendapp.vercel.app/"
-            className="text-white"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit Gym Frontend App
-          </a>
+          <p className="text-white">
+            Doctor Booking System 
+          </p>
+          <br />
+          <p className='text-gray-400 text-sm'> This full stack appointment booking system can be used by a doctor or a hospital. Because in this project we have created 3 level of authentication. 1st one is for Patients, so that patient can login on the website, book appointment with doctor and manage the booked appointment. 2nd one is doctor login, so that doctor can login and check the appointment and their earning. Doctor can update their profile also from dashboard. 3rd one is Admin Dashboard where admin can manages the appointment and admin can also manage the doctor profile.</p>
         </div>
+       
+        
 
         {/* Logistic Management System */}
         <div>
@@ -127,15 +138,47 @@ const Projects = () => {
             </div>
           </div>
           <br />
+          <p className="text-white">
+             Logistic Management System
+          </p>
+          <br />
+          <p className='text-gray-400 text-sm'> Is an online platform that streamlines logistics operations with shipment tracking, inventory management, and delivery scheduling. It offers a user-friendly interface, real-time updates, and seamless coordination across the supply chain, enhancing efficiency and reducing delays.</p>
+        </div>
+
+
+         {/* Gym Frontend App */}
+
+        <div>
+          <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+            <img src={proj1} alt="Gym Frontend" />
+            <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+              <span className="text-2xl font-bold text-white tracking-wider">Project</span>
+              <div className="pt-8 text-center">
+                <button
+                  onClick={handleGymModalToggle}
+                  className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                >
+                  Live
+                </button>
+              </div>
+            </div>
+          </div>
+          <br />
+          
           <a
-            href="https://logisticmanagementsystem.example.com/"
-            className="text-white"
+            href="https://gymfrontendapp.vercel.app/"
+            className="text-white underline"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Visit Logistic Management System
+          Check : Visit Gym Frontend App
           </a>
+          <br />
+          <br />
+          <p className='text-gray-400 text-sm'>The Gym Frontend App is a frontend application that manages the user interface for sections like Programs, Why Us, Plans, Testimonials, and a contact feature for sending questions via email. It offers a seamless experience for users to explore gym offerings, learn about the services, view membership plans, read testimonials, and easily get in touch with inquiries. With an intuitive design, the app ensures a smooth navigation experience for all users.</p>
         </div>
+
+
       </div>
 
       {/* Gym Modal */}
@@ -177,6 +220,28 @@ const Projects = () => {
           </div>
         </div>
       )}
+
+
+      {/* docBooker Modal */}
+      {isDocBookerModalOpen && (
+        <div className="fixed inset-0 bg-black/70 flex justify-center items-center z-50">
+          <div className="bg-white rounded-lg p-4 max-w-[800px] w-full">
+            <button
+              onClick={handleDocBookerModalToggle}
+              className="text-black font-bold text-lg float-right"
+            >
+              &times;
+            </button>
+            <video
+              src="/docBooker.mp4"
+              controls
+              autoPlay
+              className="w-full rounded-lg"
+            ></video>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 };
